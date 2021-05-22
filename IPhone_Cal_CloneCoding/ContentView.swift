d1dd637e9455483a0e76589385ff5e8fba45ce00
sudoswift <sudoswift@gmail.com>
2021-05-22 13:58:27 +0900

    Initial Commit

---
+++ IPhone_Cal_CloneCoding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IPhone_Cal_CloneCoding
+//
+//  Created by izvern on 2021/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IPhone_Cal_CloneCoding/IPhone_Cal_CloneCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPhone_Cal_CloneCodingApp.swift
+//  IPhone_Cal_CloneCoding
+//
+//  Created by izvern on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct IPhone_Cal_CloneCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
